be9de7f5486c7d8d413548013dd37b5dd42d1fc4
cris-2002 <gocongcristyjean07@gmail.com>
2024-11-19 15:31:50 +0800

    create.php

--- create.php
+++ create.php
@@ -1,8 +1,8 @@
 <?php
 // Database connection settings
 $servername = "localhost";
-$username = "root"; // Update with your database username
-$password = ""; // Update with your database password
+$username = "cristy"; // Update with your database username
+$password = "gocong"; // Update with your database password
 $dbname = "crud_db"; // Database name
 
 // Create connection
